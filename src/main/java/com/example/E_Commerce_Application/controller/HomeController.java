2a47930ebd13721121682e231bf93f53dcd7e439
Shiv prakash singh <sshivprakash826@gmail.com>
2026-06-08 19:59:29 +0530

    to deploy on Railway

--- src/main/java/com/example/E_Commerce_Application/controller/HomeController.java
+++ src/main/java/com/example/E_Commerce_Application/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,6 +42,12 @@ public class HomeController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Application Working";
     }
 
     @GetMapping("/Careers")
