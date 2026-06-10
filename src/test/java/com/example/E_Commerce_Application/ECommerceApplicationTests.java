ec8dd7526e0f386906a0ff6675834cfba3a2616a
SHIV PRAKASH SINGH <sshivprakash826@gmail.com>
2026-06-10 17:01:57 +0530

    Refactor ECommerceApplicationTests into main application
    Refactor ECommerceApplicationTests to ECommerceApplication with main method and SpringBootApplication annotation.

--- src/test/java/com/example/E_Commerce_Application/ECommerceApplicationTests.java
+++ src/test/java/com/example/E_Commerce_Application/ECommerceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.E_Commerce_Application;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootTest
-class ECommerceApplicationTests {
+@SpringBootApplication
+public class ECommerceApplication extends SpringBootServletInitializer {
 
-	@Test
-	void contextLoads() {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ECommerceApplication.class);
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
 }
