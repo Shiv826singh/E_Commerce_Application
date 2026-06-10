613197b66d347fa360c9237ff988b52fab6b6e2c
Shiv prakash singh <sshivprakash826@gmail.com>
2026-06-10 18:53:33 +0530

    to deploy on Railway

--- src/main/java/com/example/E_Commerce_Application/ECommerceApplication.java
+++ src/main/java/com/example/E_Commerce_Application/ECommerceApplication.java
@@ -2,18 +2,14 @@ package com.example.E_Commerce_Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 @SpringBootApplication
-public class ECommerceApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ECommerceApplication.class);
-	}
+public class ECommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
+
+        SpringApplication.run(ECommerceApplication.class, args);
+
 	}
+
+
 }
